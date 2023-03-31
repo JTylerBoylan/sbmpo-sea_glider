59e0a56b1b0bb9c69470c027d831ca9b1576268c
jtylerboylan <jtylerboylan@outlook.com>
2023-03-31 10:46:43 -0400

    Initial Work

--- my_project/include/my_project/MyCustomModel.hpp
+++ my_project/include/my_project/MyCustomModel.hpp
@@ -12,15 +12,14 @@ class MyCustomModel : public Model {
     public:
 
     // States of the Model
-    enum States {X, Y, Z, R, P, W};
+    enum States {X, Y, Q, Vx, Vy, dQdt, SoC, SG, PCMT, t};
 
     // Controls of the Model
-    enum Controls {U1, U2, U3};
+    enum Controls {dSGdt};
 
     // Constructor
     MyCustomModel() {
-        parameter1_ = 0.0f;
-        parameter2_ = std::numeric_limits<float>::infinity();
+
     }
 
     // Evaluate a node with a control
@@ -40,13 +39,11 @@ class MyCustomModel : public Model {
     // Get the cost of a control
     virtual float cost(const State& state, const Control& control, const float time_span) {
 
-        /*
-            Cost of a state and control
-            What am I trying to minimize?
-            i.e Distance, Time, Energy
-        */
+        // Maximize time at desired depth
 
-        return 0.0f;
+        float diffY = state[Y] - desired_depth_;
+
+        return cost_of_diffY_(diffY);
     }
 
     // Get the heuristic of a state
@@ -58,16 +55,20 @@ class MyCustomModel : public Model {
             What is the lowest cost possible from this state to the goal?
         */
 
+        float diffY = state[Y] - desired_depth_;
+
         return 0.0f;
     }
 
     // Determine if node is valid
     virtual bool is_valid(const State& state) {
 
-        /*
-            Does this state meet the model constraints?
-            i.e Boundary constraints, Obstacles, State limits
-        */
+        if (state[SoC] < min_energy_)
+            return false;
+
+        // Maximum depth
+        // Minimum depth
+
 
         return true;
     }
@@ -75,32 +76,53 @@ class MyCustomModel : public Model {
     // Determine if state is goal
     virtual bool is_goal(const State& state, const State& goal) {
 
-        /*
-            Is this state close enough to the goal to end the plan?
-        */
-       
-        return true;
+        // Plan ends after resurface time reached and the glider is at the surface
+
+        if (state[t] > resurface_time_ && state[Y] > surface_threshold_)
+            return true;
+
+        return false;
     }
 
     // Deconstructor
     virtual ~MyCustomModel() {}
 
-    /// @brief Set the parameter value
-    /// @param parameter1 Parameter value
-    void set_parameter1(float parameter1) {
-        parameter1_ = parameter1;
-    }
-
-    /// @brief Set the parameter value
-    /// @param parameter2 Parameter value
-    void set_parameter2(float parameter2) {
-        parameter2_ = parameter2;
-    }
-
     protected:
 
-    float parameter1_;
-    float parameter2_;
+    float gravity_;
+    float mass_;
+    float moment_;
+    float rotational_damping_;
+    float body_area_;
+    float wing_area_;
+    float desired_depth_;
+    float resurface_time_;
+    float surface_threshold_;
+    float min_energy_;
+
+
+    float power_output_(const float pcm_temp, const float depth) {
+
+        float water_temp = water_temperature_(depth);
+
+        float diffT = pcm_temp - water_temp;
+
+        // Power output
+
+        return 0.0f;
+    }
+
+    float water_temperature_(const float depth) {
+
+        // Water temp
+
+        return 0.0f;
+    }
+
+    float cost_of_diffY_(float diffY) {
+        //TODO
+        return 0.0f;
+    }
 
 
 };
